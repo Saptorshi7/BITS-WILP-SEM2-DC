221e1292b3334bb6843ad58cad3febcda5655b4d
Saptorshi7 <saptorshi98@gmail.com>
2024-03-20 19:37:32 +0000

    testing

--- final/node3/server/server_2_server.py
+++ final/node3/server/server_2_server.py
@@ -3,8 +3,8 @@ import json
 import os
 
 # Define the server address and port
-server_address = ('172.31.2.52', 12345)
-second_server_address = ('172.31.15.189', 12345)  # Address of the second server
+server_address = ('172.31.0.252', 12345)
+second_server_address = ('172.31.8.214', 12345)  # Address of the second server
 buffer_size = 1024
 timeout_seconds = 5  # Timeout for connection and response in seconds
 
@@ -16,6 +16,8 @@ server_socket.bind(server_address)
 
 # Listen for incoming connections
 server_socket.listen(1)
+
+hostname = socket.gethostname()
 
 print("Waiting for a connection...")
 
@@ -50,7 +52,7 @@ while True:
                 if os.path.exists(file_name):
                     with open(file_name, "rb") as f:
                         file_content = f.read().decode()
-                    response = {"result": file_content}
+                    response = {"result": file_content,"server": hostname}
                 elif origin < 3:  # Check origin to prevent loop
                     try:
                         # Try connecting to the second server with timeout
@@ -68,10 +70,11 @@ while True:
 
                         # Save a copy of the file received from server 2 locally
                         if "result" in response:
+                            host = response.get("server")
                             file_content = response["result"]
                             with open(file_name, "wb") as f:
                                 f.write(file_content.encode())
-
+                        print(f"File downloaded successfully from '{host}'.")
                     except (ConnectionRefusedError, socket.timeout):
                         # If connection to second server fails or timeout occurs, return file not found
                         response = {"error": "File not found"}
